2b838fbc926bdb3a0ebe67ecc723ad6bae4f0177
NataliaSaavedraM <nataliasaavedram@gmail.com>
2020-03-10 20:14:11 -0300

    Creación de  account.json

--- src/assets/components/Acces.jsx
+++ src/assets/components/Acces.jsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
+import Ellipse from "../img/Ellipse.png"
 
 const Acces =()=>{
 
   return(<Fragment>
     <p>Mensaje de Bienvenida</p>
-    <img src="" alt=""/>
+    <img src={Ellipse} alt="fotoAcceso"/>
     <button><Link to="/InicioDeSeccion">Ingresa</Link></button><button><Link to="/RegistroDatosPersonales">Registrate</Link></button>
   </Fragment>)
 }
