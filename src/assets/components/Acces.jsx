13e1b7201402ce96c56958ef801ce7f47b5c93be
NataliaSaavedraM <nataliasaavedram@gmail.com>
2020-03-11 17:56:45 -0300

    Router y botones de tvcable

--- src/assets/components/Acces.jsx
+++ src/assets/components/Acces.jsx
@@ -1,24 +1,22 @@
-import React, { Fragment } from 'react';
-import '../components/acces.css';
+import React, { Fragment } from "react";
+import "../components/acces.css";
 import Navigation from '../components/Navigation.jsx';
 import logo from '../img/ojo.png';
 
-
-const Acces =()=>{
-
-  return(
-    <Fragment >
+const Acces = () => {
+  return (
+    <Fragment>
       <div className="acces">
         <h1 className="headLine">Bienvenido a Vipago</h1>
-
-        <img src={logo} className="logo" alt="ojo parchado"/>
-        <Navigation to="/InicioDeSeccion" name="Ingresa"
-too="/RegistroDatosPersonales" namee="Regístrate"/>
+        <img src={logo} className="logo" alt="ojo parchado" />
+        <Navigation
+          to="/InicioDeSeccion"
+          name="Ingresa"
+          too="/RegistroDatosPersonales"
+          namee="Regístrate"
+        />
       </div>
-      
-
-    </Fragment>)
-
-}
+    </Fragment>
+  );
+};
 export default Acces;
-
